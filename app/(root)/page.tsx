0c4e1f0424cdbe209f22ff046336aa12d16e2572
joseph ikinda <josephikinda@gmail.com>
2025-06-02 12:07:40 +0200

    convex reglee

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,8 @@ import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import MesCategories from "@/components/MesCategories";
 
+
+
 export default function Home() {
   return (
     <div className="lg:hidden bg-myRed relative   w-full min-h-screen flex flex-col py-24">
@@ -12,9 +14,11 @@ export default function Home() {
           <p className=' font-bold text-3xl text-beige text-center'>
             Classic  made for you
           </p>
-          <p className=' font-sans text-xl  text-beige text-center'>
-            Try our ready in minute frozen meals
-          </p>
+        
+            <p className=' font-sans text-xl  text-beige text-center'>
+              Try our ready in minute frozen meals
+            </p>
+      
         </div>
         <div className="flex flex-col gap-y-3 mt-7">
           <MesCategories />
